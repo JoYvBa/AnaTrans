83b783aa09f69f6304b65ff51954cf607c7d8f61
JoYvBa <j.y.bakker@students.uu.nl>
2024-06-19 20:28:04 +0200

    Included solutions for 2 1D scenarios

--- code/OneDim.py
+++ code/OneDim.py
@@ -6,20 +6,39 @@ Created on Wed Jun 19 13:53:01 2024
 @author: jorrit
 """
 import numpy as np
+import scipy.special as sci
 
 
 class OneDim:
-    def __init__(self, method, q, n, x, t, al, D_eff = 1e-9, R = 1, mu = 0):
+    def __init__(self, method, q, n, x, t, al, m0 = 0, c0 = 0, D_eff = 0, R = 1, mu = 0):
         self.method = method
         self.q      = q
         self.n      = n
         self.v      = q/n
         self.x      = x
-        self.t      = t
+        self.t      = t[ : , np.newaxis]
+        self.m0     = m0
+        self.c0     = c0
         self.D      = D_eff + al * self.v
         self.R      = R
         self.mu     = mu
-    
-    def inf_flow_pulse(self):         # Method 1
-        result = np.zeros((len(self.x), len(self.t)))
         
+    # Method 1
+    def inf_flow_pulse(self):         
+        initial = self.m0 / (self.R * self.n * np.sqrt((4 * np.pi * self.D * self.t) / self.R))
+        exponent = np.exp((-((self.x - (self.q * self.t / (self.R * self.n)))**2) / (4*np.pi*self.D* self.t / self.R)) - ((self.mu * self.t) / self.R))
+        results = initial * exponent
+        return(results)
+        
+    # Method 3
+    def inf_flow_half(self):
+        results = 0.5 * self.c0 * sci.erfc((self.x - (self.q * self.t / (self.R * self.n))) / (4*self.D * self.t / self.R)) * np.exp((self.mu * self.t) / self.R)
+        return(results)
+    
+    def compute(self):
+        if self.method == 1:
+            return self.inf_flow_pulse()
+        elif self.method == 3:
+            return self.inf_flow_half()
+        else:
+            raise ValueError("Unknown method")
--- code/test.py
+++ code/test.py
@@ -1,10 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Created on Wed Jun 19 13:13:51 2024
 
 @author: jorrit
 """
 
-text = "Hi there, this is a test"
-print(text)
+import numpy as np
+import matplotlib.pyplot as plt
+import OneDim as D1
+
+# Time units in days
+# Distance units in meters
+# Weight units in grams
+
+method = 3
+x = np.linspace(0,100,1001)
+t = np.linspace(0,400,1001)
+q = 0.05
+n = 0.33
+al = 0.1
+m0 = 100
+c0 = 10
+R = 1
+mu = 0
+D_eff = 1e-9 * 3600 * 24
+
+transport = D1.OneDim(method = method, q = q, n = n, x = x, t = t, al = al, m0 = m0, c0 = c0, R = R, mu = mu, D_eff = D_eff)
+
+results = transport.compute()
+plt.plot(x, results[100,:], label = f"t = {np.round(t[100], 0)} days")
+plt.plot(x, results[300,:], label = f"t = {np.round(t[300], 0)} days")
+plt.plot(x, results[500,:], label = f"t = {np.round(t[500], 0)} days")
+plt.plot(x, results[700,:], label = f"t = {np.round(t[700], 0)} days")
+plt.xlabel("Distance [m]")
+plt.ylabel("Concentration [g/m3]")
+plt.legend()
+plt.show()
+
+plt.plot(t, results[:,100], label = f"x = {np.round(x[100], 0)} m")
+plt.plot(t, results[:,300], label = f"x = {np.round(x[300], 0)} m")
+plt.plot(t, results[:,500], label = f"x = {np.round(x[500], 0)} m")
+plt.plot(t, results[:,700], label = f"x = {np.round(x[700], 0)} m")
+plt.xlabel("Time [d]")
+plt.ylabel("Concentration [g/m3]")
+plt.legend()
+plt.show()
+
+sum_array = np.zeros(len(t))
+for i in range(len(sum_array)):
+    sum_array[i] = np.sum(results[i,:])
+plt.plot(t, sum_array)
