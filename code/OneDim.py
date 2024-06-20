e51818e831a1a88cb10447c69aa96de24001eb34
JoYvBa <j.y.bakker@students.uu.nl>
2024-06-20 11:54:54 +0200

    Added solutions for transport scenarios 2, 4 and 5. Improved plotting in test.

--- code/OneDim.py
+++ code/OneDim.py
@@ -10,35 +10,80 @@ import scipy.special as sci
 
 
 class OneDim:
-    def __init__(self, method, q, n, x, t, al, m0 = 0, c0 = 0, D_eff = 0, R = 1, mu = 0):
+    def __init__(self, method, q, n, x, t, al, m0 = 0, c0 = 0, D_eff = 0, R = 1, mu = 0, c1 = 0, rel_conc = True):
         self.method = method
-        self.q      = q
-        self.n      = n
-        self.v      = q/n
-        self.x      = x
-        self.t      = t[ : , np.newaxis]
-        self.m0     = m0
-        self.c0     = c0
-        self.D      = D_eff + al * self.v
-        self.R      = R
-        self.mu     = mu
+        self.q        = q
+        self.n        = n
+        self.v        = q/n
+        self.x        = x
+        self.t        = t[ : , np.newaxis]
+        self.m0       = m0
+        self.c0       = c0
+        self.D_eff    = D_eff
+        self.D        = D_eff + al * self.v
+        self.R        = R
+        self.mu       = mu
+        self.c1       = c1
+        self.rel_conc = rel_conc
+    
+    # Replace q/n in methods by v
         
-    # Method 1
+    # Method 1; Infinite column with steady flow in the x-direction and with pulse injection at t = 0
     def inf_flow_pulse(self):         
         initial = self.m0 / (self.R * self.n * np.sqrt((4 * np.pi * self.D * self.t) / self.R))
         exponent = np.exp((-((self.x - (self.q * self.t / (self.R * self.n)))**2) / (4*np.pi*self.D* self.t / self.R)) - ((self.mu * self.t) / self.R))
         results = initial * exponent
         return(results)
-        
-    # Method 3
+    
+    # Method 2; Infinite column with no flow, and with pulse injection at t = 0
+    def inf_noflow_pulse(self):
+        initial = self.m0 / (self.R * self.n * np.sqrt((4 * np.pi * self.D_eff * self.t) / self.R))
+        exponent = np.exp((-(self.x**2) / (4*np.pi*self.D_eff* self.t / self.R)) - ((self.mu * self.t) / self.R))
+        results = initial * exponent
+        return(results)
+    
+    # Method 3; Infinite column with steady flow, the solute is present with c0 at the left half of the infinite domain at t = 0
     def inf_flow_half(self):
         results = 0.5 * self.c0 * sci.erfc((self.x - (self.q * self.t / (self.R * self.n))) / (4*self.D * self.t / self.R)) * np.exp((self.mu * self.t) / self.R)
         return(results)
     
-    def compute(self):
+    # Method 4: Infinite column with steady flow, the solute is present with c0 at the left half of the infinite domain and with c1 at the right half at t = 0
+    def inf_flow_halfinit(self):
+        results = (self.c1 + 0.5 * (self.c0 - self.c1) * sci.erfc((self.x - (self.q * self.t / (self.R * self.n))) / (4*self.D * self.t / self.R)) * np.exp((self.mu * self.t)) / self.R)
+        return(results)        
+    
+    # Method 5: Semi-infinite column with steady flow and constant concentration at x = 0
+    def semi_flow_const(self):
+        # Gives problems due to large values in the exponent and small values in the erfc!!!
+        term_1 = 0.5 * self.c0
+        term_2 = self. x - (self.t * self.v / self.R)
+        term_3 = np.sqrt(4 * self.D * self.t / self.R)
+        print(term_3)
+        term_4 = np.exp(self.x * self.v / self.D)
+        print(term_4)
+        term_5 = self. x + (self.t * self.v / self.R)
+        print(term_5[-1])
+        term_23 = sci.erfc(term_2 / term_3)
+        print(term_23[-1])
+        term_53 = sci.erfc(term_5 / term_3)
+        print(term_53[-1])
+        term_453 = term_4 * term_53
+        print(term_453[-1])
+        results = term_1 * (term_23 + term_453)
+        #results = 0.5 * self.c0 * (sci.erfc((self. x - (self.t * self.v / self.R)) / (np.sqrt(4 * self.D * self.t / self.R))) + np.exp(self.x * self.v / self.D) * sci.erfc((self. x + (self.t * self.v / self.R)) / (np.sqrt(4 * self.D * self.t / self.R))))
+        return(results)
+    
+    # Streamline this with a dictionary!
+    def transport(self):
         if self.method == 1:
             return self.inf_flow_pulse()
+        elif self.method == 2:
+            return self.inf_noflow_pulse()
         elif self.method == 3:
             return self.inf_flow_half()
+        elif self.method == 4:
+            return self.inf_flow_halfinit()
+        elif self.method == 5:
+            return self.semi_flow_const()
         else:
             raise ValueError("Unknown method")
--- code/test.py
+++ code/test.py
@@ -15,40 +15,60 @@ import OneDim as D1
 # Distance units in meters
 # Weight units in grams
 
-method = 3
-x = np.linspace(0,100,1001)
-t = np.linspace(0,400,1001)
+method = 5
+
+x_min = 0
+x_max = 200
+x_nstep = 101
+t_min = 0
+t_max = 1500
+t_nstep = 101
+
 q = 0.05
 n = 0.33
 al = 0.1
 m0 = 100
 c0 = 10
+c1 = 5
 R = 1
 mu = 0
 D_eff = 1e-9 * 3600 * 24
 
-transport = D1.OneDim(method = method, q = q, n = n, x = x, t = t, al = al, m0 = m0, c0 = c0, R = R, mu = mu, D_eff = D_eff)
+plt_nx = 5
+plt_nt = 5
+fontsize = 12
 
-results = transport.compute()
-plt.plot(x, results[100,:], label = f"t = {np.round(t[100], 0)} days")
-plt.plot(x, results[300,:], label = f"t = {np.round(t[300], 0)} days")
-plt.plot(x, results[500,:], label = f"t = {np.round(t[500], 0)} days")
-plt.plot(x, results[700,:], label = f"t = {np.round(t[700], 0)} days")
-plt.xlabel("Distance [m]")
-plt.ylabel("Concentration [g/m3]")
+x = np.linspace(x_min,x_max,x_nstep)
+t = np.linspace(t_min,t_max,t_nstep)
+
+oneD = D1.OneDim(method = method, q = q, n = n, x = x, t = t, al = al, m0 = m0, c0 = c0, c1 = c1, R = R, mu = mu, D_eff = D_eff)
+results = oneD.transport()
+
+plt.figure(dpi = 300)
+
+for i in range(plt_nx):
+    time_step = int(((t_nstep - 1) / plt_nx) * (i + 1))
+    plt.plot(x, results[time_step, :], lw = 3, label = f"t = {np.round(t[time_step], 0)} days")
+
+plt.xlabel("Distance [m]", fontsize = fontsize)
+plt.ylabel("Concentration $[g m^{-3}]$", fontsize = fontsize)
+plt.title("Concentration profile", fontsize = fontsize)
 plt.legend()
 plt.show()
 
-plt.plot(t, results[:,100], label = f"x = {np.round(x[100], 0)} m")
-plt.plot(t, results[:,300], label = f"x = {np.round(x[300], 0)} m")
-plt.plot(t, results[:,500], label = f"x = {np.round(x[500], 0)} m")
-plt.plot(t, results[:,700], label = f"x = {np.round(x[700], 0)} m")
-plt.xlabel("Time [d]")
-plt.ylabel("Concentration [g/m3]")
+plt.figure(dpi = 300)
+
+for i in range(plt_nt):
+    dist_step = int(((x_nstep - 1) / plt_nt) * (i + 1))
+    plt.plot(t, results[:, dist_step], lw = 3, label = f"x = {np.round(x[dist_step], 0)} m")
+
+plt.xlabel("Time [days]", fontsize = fontsize)
+plt.ylabel("Concentration $[g m^{-3}]$", fontsize = fontsize)
+plt.title("Breakthrough curve", fontsize = fontsize)
 plt.legend()
 plt.show()
 
-sum_array = np.zeros(len(t))
-for i in range(len(sum_array)):
-    sum_array[i] = np.sum(results[i,:])
-plt.plot(t, sum_array)
+#sum_array = np.zeros(len(t))
+#for i in range(len(sum_array)):
+#    sum_array[i] = np.sum(results[i,:])
+#plt.plot(t, sum_array)
