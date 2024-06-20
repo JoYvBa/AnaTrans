6b9a077160326cf04c5544315f576eebd284440e
JoYvBa <j.y.bakker@students.uu.nl>
2024-06-20 13:53:13 +0200

    Added docstring to class

--- code/OneDim.py
+++ code/OneDim.py
@@ -10,21 +10,89 @@ import scipy.special as sci
 
 
 class OneDim:
-    def __init__(self, method, q, n, x, t, al, m0 = 0, c0 = 0, D_eff = 0, R = 1, mu = 0, c1 = 0, rel_conc = True):
+    """
+    Calculates one-dimensional analytical solutions of the Advection Dispersion Equation (ADE) for different scenarios.
+    Any units for distance, time and mass can be used, as long as they are consistant across all parameters. Output will have the same units.
+    
+    Parameters
+    ----------
+    method : Int
+        Integer representing the initial and boundary condition scenarios. See methods section for which scenarios are available and corresponding values for method.
+    q : Float
+        Darcy velocity (specific discharge), units in unit distance per unit time [L/T]
+    n : Float
+        Porosity of porous medium [-]
+    x : Array
+        Distance in the x-direction from the origin [L]. Concentration will be calculated for each distance listed in the array.
+    t : Array
+        Time [T]. Concentration will be calculated for each time listed in the array.
+    al : Float, optional
+        Longitudinal dispersitivty of the porous medium, in unit distance [L], for steady flow scenarios. Default is 0.
+    D_eff : Float, optional
+        Effective molecular diffusion coefficient [L^2/T]. The default is 0.
+    m0 : Float, optional
+        Initial mass per unit area [M/L^2] for pulse injection scenarios. The default is 0.
+    c0 : Float, optional
+        Initial concentration [M/L^3] for initial or constant concentration scenarios. The default is 0.
+    c1 : Float, optional
+        Initial concentration [M/L^3] in right half of the domain for scenario 4. The default is 0.
+    R : Float, optional
+        Linear retardation factor [-]. The default is 1.
+    mu : Float, optional
+        Linear decay rate [1/T] for scenarios with decay. The default is 0.
+   
+    Methods
+    -------
+    inf_flow_pulse() : method = 1 
+        Infinite column of porous medium with steady flow in the x-direction and a pulse input of mass per unit area m0 at t = 0.
+    inf_noflow_pulse() : method = 2 
+        Infinite column of porous medium with no flow and a pulse input of m0 at t = 0.
+    inf_flow_half() : method = 3
+        Infinite column of porous medium with steady flow in the x-direction and solute is present in the left half of the domain with concentration c0, at t = 0.
+    inf_flow_halfinit() : method = 4
+        Infinite column of porous medium with steady flow in the x-direction and solute with concentration c0 in the left half of the domain and concentration c1 in the right half of the domain at t = 0.
+    semi_flow_const() : method = 5
+        Semi-infinite column of porous medium with steady flow in the x-direction and constant concentration c0 at x = 0.
+    transport() :
+        Uses the appropiate scenario depending on the value of method.
+
+    Returns
+    -------
+    results : Array
+        Two-dimensional array containing concentration values for each combination of x and t.
+    
+    Example 
+    -------
+    (improve so everything is not on the same line)
+    
+    import OneDim as D1
+    import numpy as np
+    
+    x = np.linspace(0,200,101)
+    t = np.linspace(0,1500,101)
+
+    oneD = D1.OneDim(method = 1, q = 0.05, n = 0.33, x = x, t = t, D_eff = 1e-4, al = 1, m0 = 100, R = 1, mu = 0)
+    results = oneD.transport()
+    """
+    
+    
+    def __init__(self, method, q, n, x, t, al = 0, D_eff = 0, m0 = 0, c0 = 0, c1 = 0, R = 1, mu = 0):
         self.method = method
         self.q        = q
         self.n        = n
+        # Calculate flow velocity
         self.v        = q/n
         self.x        = x
+        # Transpose time array to the shape [n,1] for proper broadcasting
         self.t        = t[ : , np.newaxis]
+        self.D_eff    = D_eff
+        # Calculate the dispersion coefficient
+        self.D        = D_eff + al * self.v
         self.m0       = m0
         self.c0       = c0
-        self.D_eff    = D_eff
-        self.D        = D_eff + al * self.v
+        self.c1       = c1
         self.R        = R
         self.mu       = mu
-        self.c1       = c1
-        self.rel_conc = rel_conc
     
     # Replace q/n in methods by v
         
@@ -71,6 +139,8 @@ class OneDim:
         print(term_453[-1])
         results = term_1 * (term_23 + term_453)
         #results = 0.5 * self.c0 * (sci.erfc((self. x - (self.t * self.v / self.R)) / (np.sqrt(4 * self.D * self.t / self.R))) + np.exp(self.x * self.v / self.D) * sci.erfc((self. x + (self.t * self.v / self.R)) / (np.sqrt(4 * self.D * self.t / self.R))))
+        if self.rel_conc:
+            results = results / self.c0
         return(results)
     
     # Streamline this with a dictionary!
--- code/test.py
+++ code/test.py
@@ -15,7 +15,7 @@ import OneDim as D1
 # Distance units in meters
 # Weight units in grams
 
-method = 5
+method = 3
 
 x_min = 0
 x_max = 200
@@ -26,13 +26,13 @@ t_nstep = 101
 
 q = 0.05
 n = 0.33
-al = 0.1
+al = 1
 m0 = 100
 c0 = 10
 c1 = 5
 R = 1
 mu = 0
-D_eff = 1e-9 * 3600 * 24
+D_eff = 1e-4
 
 plt_nx = 5
 plt_nt = 5
@@ -41,7 +41,7 @@ fontsize = 12
 x = np.linspace(x_min,x_max,x_nstep)
 t = np.linspace(t_min,t_max,t_nstep)
 
-oneD = D1.OneDim(method = method, q = q, n = n, x = x, t = t, al = al, m0 = m0, c0 = c0, c1 = c1, R = R, mu = mu, D_eff = D_eff)
+oneD = D1.OneDim(method = method, q = q, n = n, x = x, t = t, m0 = m0, c0 = c0, c1 = c1, R = R, mu = mu, D_eff = D_eff, al = al)
 results = oneD.transport()
 
 plt.figure(dpi = 300)
