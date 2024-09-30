c3aa83083016cb1fa6ddcc47c117d5a430773bd3
JoYvBa <j.y.bakker@students.uu.nl>
2024-09-30 13:52:52 +0200

    Completed testing, added __init__.py, solved bugs

--- anatrans/ADE_eq.py
+++ anatrans/ADE_eq.py
@@ -11,13 +11,13 @@ DEF_SET = {
     "v": 1.,             # flow velocity
     "c0": 1.,            # initial concentration
     "por": 1.,           # porosity
-    "m0": 1,            # initial mass
+    "m0": 1,             # initial mass
     "deff": 1e-9,        # effective diffusion
     "alphaL": 0.1,       # dispersivity
     "alphaT": 0.01,      # dispersivity
     "alphaV": 0.001,     # dispersivity
-    "R": 1.,             # retardation rate
-    "mu": 0,             # decay rate
+    "R": 1.5,             # retardation rate
+    "mu": 0.05,           # decay rate
     "t0": 0,             # initial time
     "t1": 1,             # first time sill (e.g. end of block injection)
     "t2": 2,             # first time sill
@@ -27,7 +27,7 @@ DEF_SET = {
     }
 
 
-class cxt_1D():
+class Cxt1D:
     """Calculate concentration profile over length and time in 1D using the advection-disperison equation."""
 
     def __init__(
@@ -36,14 +36,23 @@ class cxt_1D():
         t: np.ndarray,
         **settings: float,
         ) -> None:
+        """Set up time, distance and concentration arrays.
 
-        allowed_type = [np.ndarray, float, int]
-        if ((type(x) or type(t)) not in allowed_type):
-            msg = f"The data type of x and t must both be 'numpy.ndarray', 'float' or 'int'. Types were x : {type(x)},\
+        Args:
+            x: Array with distance values.
+            t: Array with time values.
+            settings: some settings
+
+        Raises:
+            TypeError: If x or t input is not an numpy array.
+
+        """
+        if not isinstance(x, np.ndarray) or not isinstance(t, np.ndarray):
+            msg = f"The data type of x and t must both be 'numpy.ndarray'. Types were x : {type(x)},\
  t : {type(t)} instead."
-            raise Exception(msg)
+            raise TypeError(msg)
 
-        self.settings=copy.copy(DEF_SET)
+        self.settings = copy.copy(DEF_SET)
         self.settings.update(settings)
 
         self.check_settings()
@@ -65,13 +74,14 @@ class cxt_1D():
         """Calculate advection dispersion equation based on set parameters.
 
         Returns :
-            Two-dimensional array with concentration values over distance (innner dimension) and time (outer dimension).
+            cxt: Two-dimensional array with concentration values over distance (innner dimension)
+                 and time (outer dimension).
         """
         self.settings.update(settings)
         self.check_settings()
 
         if retardation:
-            t = self.t * self.settings["R"]
+            t = self.t / self.settings["R"]
             m0 = self.settings["m0"] / self.settings["R"]
         else:
             t = self.t
@@ -82,12 +92,12 @@ class cxt_1D():
         else:
             d = self.settings["deff"]
 
-        adv = self.x - self.settings["v"] * t if advection else self.x
+        self.adv = self.x - self.settings["v"] * t if advection else self.x
 
-        spread = d * t
+        self.spread = d * t
 
-        print(np.exp(-((adv)**2 / (4 * spread))))
-        self.cxt = m0 / (self.settings["por"] * np.sqrt(4 * np.pi * spread)) * np.exp(-((adv)**2 / (4 * spread)))
+        self.cxt = m0 / (self.settings["por"] * np.sqrt(4 * np.pi * self.spread)) \
+            * np.exp(-((self.adv)**2 / (4 * self.spread)))
 
         if decay:
             self.cxt *= np.exp(-self.settings["mu"] * t)
@@ -95,10 +105,17 @@ class cxt_1D():
         return self.cxt
 
     def check_settings(self) -> None:
+        """Give an error if the settings have incorrect value or data type.
+
+        Raises:
+            TypeError: If data type of parameters is not an integer or float.
+            ValueError: If data type of parameters is smaller than 0.
+
+        """
         for key, value in self.settings.items():
             if type(value) not in {float, int}:
                 msg = f"The data type of {key} must be 'float' or 'int', not {type(value)}"
-                raise ValueError(msg)
+                raise TypeError(msg)
 
             if value < 0.:
                 msg = f"The value of {key} is {value}, but must be larger than or equal to 0"
--- tests/test_ADE.py
+++ tests/test_ADE.py
@@ -1,10 +1,12 @@
 import sys
 
-sys.path.insert(0, "/Users/jorrit/anatrans/anatrans")
+sys.path.insert(0, "/Users/jorrit/anatrans")
 
-import ADE_eq as ade
 import numpy as np
 import pytest
+
+import anatrans.ADE_eq as ade
+from tests.output_testing import ADE_results
 
 
 class TestClassOneDim:
@@ -12,33 +14,57 @@ class TestClassOneDim:
 
     def setup_method(self):
         self.x_min = 0
-        self.x_max = 200
-        self.x_nstep = 51
+        self.x_max = 20
+        self.x_nstep = 11
         self.t_min = 0
-        self.t_max = 1500
-        self.t_nstep = 51
+        self.t_max = 10
+        self.t_nstep = 11
 
         self.x = np.linspace(self.x_min, self.x_max, self.x_nstep)
         self.t = np.linspace(self.t_min, self.t_max, self.t_nstep)
-
-    # def test_inf_flow_pulse(self) -> None:
-
-    #     # results = D1.transport(method = method, x = self.x, t = self.t, D_eff = self.D_eff, c0 = self.c0)
-    #     # assert all(results[1:,0] == self.c0)
-    #     # print(results[50,25])
-    #     # assert results[50,25] == pytest.approx(5.90636)
+        self.adv = ade.Cxt1D(self.x, self.t)
 
     def test_value_type(self) -> None:
         """Test to make sure that wrong input data types are recognized."""
-        adv = ade.cxt_1D(self.x, self.t)
         v = "This should not work"
-        with pytest.raises(ValueError, match=f"The data type of v must be 'float' or 'int', not {type(v)}"):
-            adv.ade(v=v)
+        with pytest.raises(TypeError, match=f"The data type of v must be 'float' or 'int', not {type(v)}"):
+            self.adv.ade(v=v)
 
     def test_value_range(self) -> None:
         """Test to make sure that negative input data is not accepted."""
-        adv = ade.cxt1D(self.x, self.t)
         v = -0.8
         with pytest.raises(ValueError, match=f"The value of v is {v}, but must be larger than or equal to 0"):
-            adv.ade(v=v)
+            self.adv.ade(v=v)
 
+    def test_input_type(self) -> None:
+        """Test to make sure that only numpy arrays are accepted as input."""
+        self.x = 10
+        with pytest.raises(TypeError, match=f"The data type of x and t must both be 'numpy.ndarray'.\
+ Types were x : {type(self.x)}, t : {type(self.t)} instead."):
+            ade.Cxt1D(self.x, self.t)
+
+    def test_tile(self) -> None:
+        """Test to make sure distance, time and concentration tiles are correctly generated."""
+        x = np.tile(self.x, (len(self.t), 1))
+        t = np.tile(self.t, (len(self.x), 1)).T
+        cxt = np.zeros((len(self.t), len(self.x)))
+        out = ade.Cxt1D(self.x, self.t)
+
+        np.testing.assert_allclose(x, out.x)
+        np.testing.assert_allclose(t, out.t)
+        np.testing.assert_allclose(cxt, out.cxt)
+
+    def test_ade_noadv(self) -> None:
+        """Test to make sure variables are correctly made when calculating the ADE without advection."""
+        self.adv.ade(advection=False)
+        np.testing.assert_allclose(self.adv.adv, self.adv.x)
+
+    def test_ade_advdispdec(self) -> None:
+        """Test to make sure ADE is correctly calculated with advection, dispersion and decay."""
+        self.adv.ade(advection=True, dispersion=True, decay=True)
+        np.testing.assert_allclose(self.adv.cxt[-1, :], ADE_results["adv_disp_decay_pulse"])
+
+    def test_ade_advdispdecret(self) -> None:
+        """Test to make sure ADE is correctly calculated with advection, dispersion, decay and retardation."""
+        self.adv.ade(advection=True, dispersion=True, decay=True, retardation=True)
+        np.testing.assert_allclose(self.adv.cxt[-1, :], ADE_results["adv_disp_decay_retard_pulse"])
